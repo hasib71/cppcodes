ff5077748e21b5217e71f1a7261137d6327b122d
Hasib Al Muhaimin <himuhasib@gmail.com>
2015-01-13 01:26:02 +0600

    More Contest

---
+++ CodeForces/Codeforces Round #283 (Div. 2)/c.cpp
@@ -0,0 +1,136 @@
+/****************************************************************
+   ▄█    █▄       ▄████████    ▄████████  ▄█  ▀█████████▄
+  ███    ███     ███    ███   ███    ███ ███    ███    ███
+  ███    ███     ███    ███   ███    █▀  ███   ███    ███
+ ▄███▄▄▄▄███▄▄   ███    ███   ███        ███  ▄███▄▄▄██▀
+▀▀███▀▀▀▀███▀  ▀███████████ ▀███████████ ███ ▀▀███▀▀▀██▄
+  ███    ███     ███    ███          ███ ███    ███    ██▄
+  ███    ███     ███    ███    ▄█    ███ ███    ███    ███
+  ███    █▀      ███    █▀   ▄████████▀  █▀   ▄█████████▀
+****************************************************************/
+
+
+
+#include<bits/stdc++.h>
+
+
+#define FOR(i, s, e) for(int i=s; i<e; i++)
+#define loop(i, n) FOR(i, 0, n)
+#define sf scanf
+#define pf printf
+#define pb push_back
+#define MP make_pair
+#define fr first
+#define sc second
+#define ll long long
+#define dd double
+#define all(v) v.begin(), v.end()
+#define PI acos(-1.0)
+#define mem(ara, value) memset(ara, value, sizeof(ara))
+#define paii pair<int, int>
+#define pall pair<ll, ll>
+#define SZ(a) int(a.size())
+#define read(nm) freopen(nm, "r", stdin)
+#define write(nm) freopen(nm, "w", stdout)
+
+#define dump(x) cerr<<#x<<" = "<<x<<endl
+#define debug(args...) cerr,args; cerr<<endl;
+using namespace std;
+
+
+template<typename T>
+ostream& operator<<(ostream& output, vector<T>&v)
+{
+    output<<"[ ";
+    if(SZ(v))
+    {
+        output<<v[0];
+    }
+    FOR(i, 1, SZ(v))
+    {
+        output<<", "<<v[i];
+    }
+    output<<" ]";
+    return output;
+}
+
+template<typename T1, typename T2>
+ostream& operator<<(ostream& output, pair<T1, T2>&p)
+{
+    output<<"( "<<p.fr<<", "<<p.sc<<" )";
+    return output;
+}
+
+
+
+
+template<typename T>
+ostream& operator,(ostream& output, T x)
+{
+    output<<x<<" ";
+    return output;
+}
+
+
+
+
+
+//Header ends here
+
+#define MAXX 107
+
+int n, m;
+
+char matrix[MAXX][MAXX];
+
+int fix[MAXX];
+
+
+int main()
+{
+    cin>>n>>m;
+
+    loop(i, n)
+    {
+        cin>>matrix[i];
+    }
+
+
+    int toRemove = 0;
+
+    loop(j, m)
+    {
+        bool acceptable = true;
+
+        for(int i=1; i<n; i++)
+        {
+            if(matrix[i-1][j] > matrix[i][j] && !fix[i])
+            {
+                acceptable = false;
+                break;
+            }
+        }
+
+        if(acceptable)
+        {
+            for(int i=1; i<n; i++)
+            {
+                if(matrix[i-1][j] < matrix[i][j])
+                {
+                    fix[i] = true;
+                }
+            }
+        }
+        else
+        {
+            toRemove++;
+        }
+    }
+
+    cout<<toRemove<<endl;
+
+
+
+
+
+}
